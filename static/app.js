5a9e8f4d242e3b251dffe12e53d4c810e488998f
Kaido Iwamoto <h6824.kaido@gmail.com>
2017-11-15 18:47:12 +0900

    Disable antialiasing and support thick line

--- static/app.js
+++ static/app.js
@@ -9,6 +9,82 @@
     node.addEventListener(name, func);
   }
 
+  function plot(imageData, x, y, r, g, b, a) {
+    if (x < 0 || y < 0 || x >= imageData.width || y >= imageData.height) {
+      return;
+    }
+    let i = (x + imageData.width * y) * 4;
+    imageData.data[i] = r;
+    imageData.data[i + 1] = g;
+    imageData.data[i + 2] = b;
+    imageData.data[i + 3] = a;
+  }
+
+  function drawLine(ctx, x0, y0, x1, y1, thickness) {
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    plot(imageData, x0, y0, 0, 0, 0, 255);
+    let steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
+    if (steep) {
+      [x0, y0] = [y0, x0];
+      [x1, y1] = [y1, x1];
+    }
+    if (x0 > x1) {
+      [x0, x1] = [x1, x0];
+      [y0, y1] = [y1, y0];
+    }
+    let deltaX = x1 - x0;
+    let deltaY = Math.abs(y1 - y0);
+    let error = deltaX >> 1;
+    let stepY = y0 < y1 ? 1 : -1;
+    let y = y0;
+    let ps = [];
+    {
+      let _y = 0, _e = deltaX >> 1;
+      for (let _x = 1; _x < thickness; _x++) {
+        ps.push([_y, -_x]);
+        _e -= deltaY;
+        if (_e < 0) {
+          _e += deltaX;
+          _y += stepY;
+        }
+      }
+    }
+    for (let x = x0; x <= x1; x++) {
+      if (steep) {
+        plot(imageData, y, x, 0, 0, 0, 255);
+        ps.forEach(([vx, vy]) => {
+          plot(imageData, y + vy, x + vx, 0, 0, 0, 255);
+          plot(imageData, y - vy, x - vx, 0, 0, 0, 255);
+        });
+      } else {
+        plot(imageData, x, y, 0, 0, 0, 255);
+        ps.forEach(([vx, vy]) => {
+          plot(imageData, x + vx, y + vy, 0, 0, 0, 255);
+          plot(imageData, x - vx, y - vy, 0, 0, 0, 255);
+        });
+      }
+      error -= deltaY;
+      if (error < 0) {
+        error += deltaX;
+        y += stepY;
+        if (steep) {
+          ps.forEach(([vx, vy]) => {
+            plot(imageData, y + vy, x + vx, 0, 0, 0, 255);
+            plot(imageData, y - vy, x - vx, 0, 0, 0, 255);
+          });
+        } else {
+          ps.forEach(([vx, vy]) => {
+            plot(imageData, x + vx, y + vy, 0, 0, 0, 255);
+            plot(imageData, x - vx, y - vy, 0, 0, 0, 255);
+          });
+        }
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+  }
+
   on(window, 'load',() => {
     const canvas = d.getElementById('canvas');
     if (canvas) {
@@ -32,10 +108,7 @@
       });
       on(canvas, 'mousemove', (event) => {
         if (prevPoint) {
-          ctx.beginPath();
-          ctx.moveTo(prevPoint.x, prevPoint.y);
-          ctx.lineTo(event.offsetX, event.offsetY);
-          ctx.stroke();
+          drawLine(ctx, prevPoint.x, prevPoint.y, event.offsetX, event.offsetY, 2);
           prevPoint = {x: event.offsetX, y: event.offsetY};
         }
       });
