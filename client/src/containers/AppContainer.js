1d798c76e15c08240a1e476b09b5941313a3043c
Kaido Iwamoto <h6824.kaido@gmail.com>
2017-12-07 00:00:33 +0900

    Make container functinal

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -7,46 +7,20 @@ import PictureStore from '../data/PictureStore';
 import RouteStore from '../data/RouteStore';
 import history from '../history';
 
-function getStores() {
-}
+export default Container.createFunctional(
+  App,
+  () => ([
+    PictureStore,
+    RouteStore,
+  ]),
+  () => ({
+    picture: PictureStore.getState(),
+    route: RouteStore.getState(),
 
-function getState() {
-}
-
-class AppContainer extends Component {
-  static getStores() {
-    return [
-      PictureStore,
-      RouteStore,
-    ];
-  }
-
-  static calculateState() {
-    return {
-      picture: PictureStore.getState(),
-      route: RouteStore.getState(),
-
-      pushHistory: DrawerActions.pushHistory,
-      onMouseDown: DrawerActions.handleOnMouseDown,
-      onMouseMove: DrawerActions.handleOnMouseMove,
-      onMouseUp: DrawerActions.handleOnMouseUp,
-      clearCanvas: DrawerActions.clearCanvas,
-    };
-  }
-
-  static childContextTypes = {
-    history: PropTypes.object.isRequired,
-  };
-
-  getChildContext() {
-    return {
-      history,
-    };
-  }
-
-  render() {
-    return <App {...this.state}/>
-  }
-}
-
-export default Container.create(AppContainer);
+    pushHistory: DrawerActions.pushHistory,
+    onMouseDown: DrawerActions.handleOnMouseDown,
+    onMouseMove: DrawerActions.handleOnMouseMove,
+    onMouseUp: DrawerActions.handleOnMouseUp,
+    clearCanvas: DrawerActions.clearCanvas,
+  })
+);
